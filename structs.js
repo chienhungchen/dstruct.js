d492881c673dc9c876d7523da87e1a19401a9c92
Chien-Hung Chen <cxc330@case.edu>
2012-02-01 15:54:06 -0500

    first commit to the dstruct.js project

---
+++ structs.js
@@ -0,0 +1,57 @@
+/******
+* structs.js
+* Created by: Chien-Hung Chen and Christopher Gross
+* Created with the intent of offering special data structures for javascript users
+*/
+
+/******
+*	Queue - FIFO
+*	Methods implemented:
+*		enqueue, push, add; dequeue, pop, remove; clear; size, length, count; contains; isempty; peek, front; back
+*/
+function Queue(){
+
+	//initializing the queue
+	var queue = [];
+
+	//enqueue and push adds the object on to the end
+	this.enqueue = function(obj) { queue.push(obj); }
+	this.push = this.enqueue;
+	this.add = this.enqueue;
+	
+	//dequeues an obj and returns the object dequeued
+	this.dequeue = function() {
+		if (queue.length == 0)
+			return undefined;
+		var obj = queue[0];
+		queue = queue.slice(1);
+		return obj;
+	}
+	this.pop = this.dequeue;
+	this.remove = this.dequeue;
+	
+	//empty the queue, there's a lot of discussion as to whether to use q.length = 0 or q = []; see: http://jsperf.com/emptying-arrays
+	this.clear = function() { queue.length = 0; }
+	
+	//return length of the queue
+	this.size = function() { return (queue.length); }
+	this.length = this.size;
+	this.count = this.size;
+	
+	//returns whether or not the queue contains obj
+	this.contains = function(obj) {
+		for(o in queue) { if(queue[o] == obj) return true; }
+		return false;
+	}
+	
+	//return whether or not the queue is empty
+	this.isempty = function() { return queue.length == 0; }
+
+	//returns the item at the front of the queue without dequeuing it
+	this.peek = function() { return ((queue.length > 0) ? queue[0] : undefined); }
+	this.front = this.peek;
+	
+	//return the item at the end of the queue
+	this.back = function() { return ((queue.length > 0) ? queue[queue.length - 1] : undefined); }
+
+}
